aa805a1f92305005cd5b1a884bbeb005c8084a6f
Chris Babb <ccbabb@gmail.com>
2024-05-06 21:32:39 -0500

    2nd commit

--- db/schema.sql
+++ db/schema.sql
@@ -20,5 +20,7 @@ CREATE TABLE employees (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    
+    role_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -9,17 +9,54 @@ const pool = new Pool({
   user: "postgres",
   password: "bobcat",
   network: "localhost",
-  database: "",
+  database: "employees_db",
 });
 
-inquirer.prompt([
-  {
-    type: "list",
-    message: "What would you like to do?",
-    name: "action",
-    choices: [],
-  },
-]);
+const asciiArt = `
+
+_______________________________________________________________________________________________
+|    ______                 _                         __  __                                   |
+|   |  ____|               | |                       |  \\/  |                                  |
+|   | |__   _ __ ___  _ __ | | ___  _   _  ___  ___  | \\  / | __ _ _ __   __ _  __ _  ___ _ __ |
+|   |  __| | '_   _ \\| '_ \\| |/ _ \\| | | |/ _ \\/ _ \\ | |\\/| |/ _  | '_ \\ / _  |/ _  |/ _ \\ '__||
+|   | |____| | | | | | |_) | | (_) | |_| |  __/  __/ | |  | | (_| | | | | (_| | (_| |  __/ |   |
+|   |______|_| |_| |_| .__/|_|\\___/ \\__, |\\___|\\___| |_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|   |
+|                    | |             __/ |                                      __/ |          |
+|                    |_|            |___/                                      |___/           |
+|______________________________________________________________________________________________|
+
+`;
+
+console.log(asciiArt);
+
+inquirer
+  .prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      name: "action",
+      choices: [
+        "View All Employees",
+        "Add Employee",
+        "Update Employee Role",
+        "View All Roles",
+        "Add Role",
+        "View All Departments",
+        "Add Department",
+        "Quit",
+      ],
+    },
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
 
 pool.connect();
 
